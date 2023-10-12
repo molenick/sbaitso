17bc2c15392d2702f4ed339f55fc5073305bfe74
Matt Olenick <molenick@users.noreply.github.com>
2023-10-11 17:31:24 -0700

    Make temperature goal/level

--- src/bin/sbaitso.rs
+++ src/bin/sbaitso.rs
@@ -11,47 +11,47 @@ slint::include_modules!();
 
 #[derive(Clone)]
 struct State {
-    power_goal: f32,
-    power_level: f32,
-    power_goal_announced: bool,
+    temperature_goal: f32,
+    temperature_level: f32,
+    temperature_goal_announced: bool,
 }
 impl State {
     fn new() -> Self {
         Self {
-            power_goal: 0.0,
-            power_level: 0.0,
-            power_goal_announced: false,
+            temperature_goal: 0.0,
+            temperature_level: 0.0,
+            temperature_goal_announced: false,
         }
     }
 
-    fn set_power_goal(&mut self, goal: f32) {
-        self.power_goal = goal;
+    fn set_temperature_goal(&mut self, goal: f32) {
+        self.temperature_goal = goal;
     }
 
-    // todo make decay when power goal is lower
+    // todo make decay when temperature goal is lower
     fn update(&mut self) {
-        self.power_level += self.power_goal * 0.005;
-        self.power_level = self.power_level.clamp(0.0, self.power_goal);
+        self.temperature_level += self.temperature_goal * 0.005;
+        self.temperature_level = self.temperature_level.clamp(0.0, self.temperature_goal);
 
-        if !self.power_goal_reached() {
-            self.power_goal_announced = false;
+        if !self.temperature_goal_reached() {
+            self.temperature_goal_announced = false;
         }
     }
 
-    fn power_goal_reached(&self) -> bool {
-        self.power_level == self.power_goal && self.power_level > 0.0
+    fn temperature_goal_reached(&self) -> bool {
+        self.temperature_level == self.temperature_goal && self.temperature_level > 0.0
     }
 }
 
 fn update_gui(weak_window: Weak<MainWindow>, state: Arc<Mutex<State>>) {
-    // hmm, power_goal update was prob broken bc this got copied once but not iteratively
+    // hmm, temperature_goal update was prob broken bc this got copied once but not iteratively
     // TODO Fix
     loop {
         let s = state.clone();
-        let power_level = s.lock().unwrap().power_level;
+        let temperature_level = s.lock().unwrap().temperature_level;
         weak_window
             .upgrade_in_event_loop(move |window| {
-                window.set_power_level(power_level);
+                window.set_temperature_level(temperature_level);
             })
             .unwrap();
 
@@ -72,8 +72,11 @@ fn main() -> Result<(), Error> {
     let weak_window: Weak<MainWindow> = window.as_weak();
 
     let set_power_goal_state = state.clone();
-    window.on_power_goal_changed(move |goal| {
-        set_power_goal_state.lock().unwrap().set_power_goal(goal);
+    window.on_temperature_goal_changed(move |goal| {
+        set_power_goal_state
+            .lock()
+            .unwrap()
+            .set_temperature_goal(goal);
     });
 
     let gui_state = state.clone();
@@ -82,13 +85,14 @@ fn main() -> Result<(), Error> {
     let tts_state = state.clone();
     rt.spawn(async move {
         loop {
-            if tts_state.lock().unwrap().power_goal_reached()
-                && !tts_state.lock().unwrap().power_goal_announced
+            if tts_state.lock().unwrap().temperature_goal_reached()
+                && !tts_state.lock().unwrap().temperature_goal_announced
             {
-                let power_level = tts_state.lock().unwrap().power_level.round();
-                let announcement = format!("Power goal reached: {power_level} degrees fahrenheit");
+                let temperature_level = tts_state.lock().unwrap().temperature_level.round();
+                let announcement =
+                    format!("Power goal reached: {temperature_level} degrees fahrenheit");
                 tts.speak(announcement, false).unwrap();
-                tts_state.lock().unwrap().power_goal_announced = true;
+                tts_state.lock().unwrap().temperature_goal_announced = true;
             }
 
             thread::sleep(Duration::from_millis(10));
